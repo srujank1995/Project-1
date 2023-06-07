65e2d693039872c58c616a5580ad7d24a37beae7
sru_kinj1995 <srujankinjawadekar1@gmail.com>
2023-06-07 23:28:04 +0530

    validation added

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,44 +1,37 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { LoginUser } from "../../mainStore/UserStore/User.action";
-import { useNavigate } from "react-router-dom";
+
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const dispatch: any = useDispatch();
-  const navigate = useNavigate()
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log(email, password);
-    const info = {
-      email,
-      password,
-    }
-    dispatch(
-      LoginUser(navigate, info)
-    );
-    // Validation logic //
-    const validationErrors: { [key: string]: string } = {};
-    if (!email) {
-      validationErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      validationErrors.email = "Email is invalid";
-    }
+    emailvalidate(email)
+    passwordvalidate(password)
+    console.log(email, password)
 
-    if (!password) {
-      validationErrors.password = "Password is required";
-    }
-    if (Object.keys(validationErrors).length > 0) {
-      setErrors(validationErrors);
-    } else {
-      // Submit form logic
-      alert("Form Submitted Successfully");
-    }
-  };
+  }
 
+  const emailvalidate =(value:string) =>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!value){
+      setEmail('Email is required')
+    }else if(!emailRegex.test(value)){
+      setEmail("Invalid Email")
+    }else{
+      setEmail('')
+    }}
+  
+  const passwordvalidate =(value:string) =>{
+    if(!value){
+      setPassword('Password Required')
+    }else if(value.length < 6){
+      setPassword('Password should be atleast more than 6 letter')
+    }else{
+      setPassword('')
+    }
+   }
   return (
     <div className=" bg-blue-400 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -53,7 +46,7 @@ const Login: React.FC = () => {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="email"
@@ -66,15 +59,12 @@ const Login: React.FC = () => {
                 id="email"
                 type="email"
                 autoComplete="email"
+                value={email}
                 required
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
+                onChange={(e) => {setEmail(e.target.value)}}
+                onBlur={(e)=>{emailvalidate(e.target.value)}}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-              {errors.email && (
-                <p className="text-red-500 text-xs mt-1">{errors.email}</p>
-              )}
             </div>
           </div>
 
@@ -100,16 +90,14 @@ const Login: React.FC = () => {
                 id="password"
                 name="password"
                 type="password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
+                value={password}
+                onChange={(e)=>{setPassword(e.target.value)}}
+                onBlur={(e)=>{passwordvalidate(e.target.value)}}
                 autoComplete="current-password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-              {errors.password && (
-                <p className="text-red-500 text-xs mt-1">{errors.password}</p>
-              )}
+             
             </div>
           </div>
 
@@ -120,6 +108,7 @@ const Login: React.FC = () => {
                bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6
                 text-white shadow-sm hover:bg-red-900 focus-visible:outline 
                 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
+                onSubmit={handleSubmit}
             >
               Sign in
             </button>
